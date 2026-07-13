e120b86476e8258a48868fff7ff9fa16c1f89848
nguyenla171120 <247720863+nguyenla171120@users.noreply.github.com>
2026-07-13 10:01:25 +0700

    revert format

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,4 +1,4 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient } from '@tanstack/react-query'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,6 +11,6 @@ const queryClient = new QueryClient({
       retry: 0,
     },
   },
-});
+})
 
-export default queryClient;
+export default queryClient
