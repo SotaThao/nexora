94b555b365dbfd33b2ef2ea7f9c8ebe4e73442e5
nguyenla171120 <247720863+nguyenla171120@users.noreply.github.com>
2026-07-13 09:57:53 +0700

    disable refetch apis

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,15 +1,16 @@
-import { QueryClient } from '@tanstack/react-query'
+import { QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       gcTime: 5 * 60_000,
       retry: 1,
+      refetchOnWindowFocus: false,
     },
     mutations: {
       retry: 0,
     },
   },
-})
+});
 
-export default queryClient
+export default queryClient;
